4313237cc65c50078f84b7d323788e53f50680f5
Rhodri-hc <13622450494@163.com>
2022-06-15 09:34:39 +0800

    响应丢失问题

--- src/responsive/ref.js
+++ src/responsive/ref.js
@@ -23,3 +23,46 @@ function ref(val) {
     // 将包裹对象变成响应式数据
     return reactive(wrapper);
 }
+
+/**
+* @desc 解决响应丢失问题
+* @author 张和潮
+* @date 2022年06月15日 09:19:57
+*/
+function toRef(obj, key){
+    // 定义访问器属性
+    const wrapper = {
+        get value(){
+            return obj[key];
+        },
+
+        set value(newVal){
+            obj[key] = newVal
+        }
+    }
+
+    // 定义_v_isRef 属性
+    Object.defineProperty(wrapper, '_v_isRef', {
+        value: true
+    })
+
+    return wrapper
+
+}
+
+/**
+* @desc 批量完成响应转换
+* @author 张和潮
+* @date 2022年06月15日 09:24:00
+*/
+function toRefs(obj) {
+    const ret = {}
+
+    // 使用for...in 循环遍历对象
+    for (const key in obj) {
+        // 逐个调用 toRef 完成转换
+        ret[key] = toRef(obj, key)
+    }
+
+    return ret;
+}

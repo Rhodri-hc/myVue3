db2b93248517be181cde08e149a7f6b55db04a06
Rhodri-hc <13622450494@163.com>
2022-06-15 22:11:04 +0800

    渲染器与响应系统的结合

---
+++ src/renderer/createRenderer.js
@@ -0,0 +1,14 @@
+/**
+ * 渲染器的作用：把虚拟 DOM 对象渲染为真实的 DOM 元素。
+ * 工作原理：递归遍历虚拟对象，并调用原生DOM API 来完成真实 DOM 的创建。
+ *          更新的时候，会通过Diff算法找出变更点，并且只会更新需要更新的内容。
+ * 
+ */
+/**
+* @desc 渲染器
+* @author 张和潮
+* @date 2022年06月15日 22:03
+*/
+function renderer(domString, container) {
+    container.innerHTML = domString
+}

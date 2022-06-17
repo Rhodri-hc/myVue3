69503e6c644f53a55e1b804a4c0491c5e0de98d1
Rhodri-hc <13622450494@163.com>
2022-06-17 15:49:19 +0800

    区分vNode类型

--- src/renderer/createRenderer.js
+++ src/renderer/createRenderer.js
@@ -131,14 +131,31 @@ function createRenderer(options) {
     * @date 2022年06月16日 15:31:32
     */
     function patch(n1, n2, container) {
-        // 如果n1 不存在，意味着挂载，则调用 mountElement 函数完成挂载
-        if (!n1) {
-            mountElement(n2, container)
-        } 
-        // n1 存在，意味着打补丁
-        else{
-
+        // 如果n1 存在，则对比 n1 和 n2 的类型
+        if (n1 && n1.type !== n2.type) {
+            // 如果新旧 vNode 的类型不同，直接将旧vNode 卸载
+            unmount(n1);
+            n1 = null;
         }
+        
+        // 代码运行到这里，证明 n1 和 n2 所描述的内容相同
+        const { type } = n2;
+        // 如果 n2.type 的值是字符串类型，则它描述的是普通标签元素
+        if (typeof type === "string") {
+            // 如果n1 不存在，意味着挂载，则调用 mountElement 函数完成挂载
+            if (!n1) {
+                mountElement(n2, container)
+            } 
+            // n1 存在，意味着打补丁
+            else{
+                // patchElement(n1, n2)
+            }
+            
+        } else if (typeof type === "object"){
+            // 如果n2.type 的值的类型是对象，则它描述的是组件
+
+        } 
+        // ...处理其他类型的node
     }
 
 

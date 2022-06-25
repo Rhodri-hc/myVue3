6f6d5ea867d527764a5b11045805a36e2546ccfc
Rhodri-hc <13622450494@163.com>
2022-06-25 17:30:42 +0800

    快速diff算法

--- src/renderer/createRenderer.js
+++ src/renderer/createRenderer.js
@@ -280,6 +280,73 @@ function createRenderer(options) {
      */
 
     /**
+    * @desc 快速 Diff 算法
+    * @params { Object } n1 旧vNode
+    * @params { Object } n2 新vNode
+    * @params { Object } container dom
+    * @author 张和潮
+    * @date 2022年06月25日 16:39:29
+    */
+   function quickDiff(n1, n2, container) {
+        const newChildren = n2.children;
+        const oldChildren = n1.children;
+
+        // 更新相同的前置节点
+        // 索引 j 指向新旧两组子节点的开头
+        let j = 0;
+        let oldVNode = oldChildren[j];
+        let newVNode = newChildren[j];
+
+        // while 循环向后遍历，直到遇到拥有不同的key值的节点为止
+        while(oldVNode.key === newVNode.key){
+            // 调用patch 函数进行更新
+            patch(oldVNode, newVNode, container);
+            // 更新索引j，让其递增
+            j++;
+            oldVNode = oldChildren[j];
+            newVNode = newChildren[j];
+        }
+
+        // 更新相同的后置节点
+        // 索引 oldEnd 指向旧的一组子节点的最后一个节点
+        let oldEnd = oldChildren.length - 1;
+        // 索引 newEnd 指向新的一组子节点的最后一个节点
+        let newEnd = newChildren.length - 1;
+
+        oldVNode = oldChildren[oldEnd];
+        newVNode = newChildren[newEnd];
+
+        // while 循环从后向前遍历，直到遇到拥有不同key值的节点为止
+        while (oldVNode.key === newVNode.key) {
+            // 调用patch 函数进行更新
+            patch(oldVNode, newVNode, container);
+            // 递减 oldEnd 和 newEnd
+            oldEnd--;
+            newEnd--;
+            oldVNode = oldChildren[oldEnd];
+            newVNode = newChildren[newEnd];
+        }
+
+        // 预处理完毕后，如果满足条件 j --> newEnd 之间的节点应作为新节点插入
+        if (j > oldEnd && j < newEnd) {
+            // 锚点索引
+            const anchorIndex = newEnd + 1;
+            // 锚点元素
+            const anchor = anchorIndex < newChildren.length ? newChildren[anchorIndex].el : null;
+            // 采用 while 循环，调用 patch 函数逐个挂载新增节点
+            while(j <= newEnd){
+                patch(null, newChildren[j++], container, anchor);
+            }
+        } else if (j > newEnd && j <= oldEnd) {
+            // j -> oldEnd 之间的节点应该被卸载
+            while (j <= oldEnd) {
+                unmount(oldChildren[j++])
+            } 
+        }
+   }
+
+
+    /**
     * @desc 双端 diff 算法
     * @params { Object } n1 旧vNode
     * @params { Object } n2 新vNode

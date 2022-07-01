bc498692d38124dafd1d7d69ca5859e23bddc753
Rhodri-hc <13622450494@163.com>
2022-07-01 11:20:29 +0800

    重试加载组件

--- src/renderer/asyncComponent.js
+++ src/renderer/asyncComponent.js
@@ -27,6 +27,36 @@ function defineAsyncComponent(options){
 
     // 用来存储异步加载的组件
     let InnerComp = null;
+
+    // 记录重试次数
+    let retries = 0;
+
+    // 封装 load 函数用来加载异步组件
+    function load() {
+        return loader()
+        // 捕获加载器的错误
+        .catch((err) => {
+            // 如果用户指定了 onError 回调，则将控制权交给用户
+            if (options.onError) {
+                // 返回一个新的Promise 实例
+                return new Promise((resolve, reject) => {
+                    // 重试
+                    const retry = () => {
+                        resolve(load())
+                        retries++; 
+                    }
+                    // 失败
+                    const fail = () => reject(err)
+                    // 作为 onError 回调函数的参数，让用户来决定下一步该怎么做
+                    options.onError(retry, fail)
+                })
+            } else{
+               throw err;
+            }
+        })
+    }
+
+
     // 返回一个包装组件
     return {
         name: 'AsyncComponentWrapper',
@@ -51,21 +81,27 @@ function defineAsyncComponent(options){
                 loading.value = true;
             }
 
+
             // 执行加载器函数，返回一个 Promise 实例
             // 加载成功后，将加载成功的组件赋值给InnerComp，并将loaded 标记为true，
             // 代表加载成功
-            loader().then(c => {
-                InnerComp = c;
-                loaded.value = true;
-            })
-            .catch((err) => {
-                error.value = err;
-            })
-            .finally(() => {
-                loading.value = false;
-                // 加载完毕后，无论成功与否都要清除延迟定时器
-                clearTimeout(loadingTimer);
-            })
+
+            // 调用load 加载函数
+            load()
+                .then(c => {
+                    InnerComp = c;
+                    loaded.value = true;
+                })
+                .catch((err)=>{
+                    error.value = err
+                })
+                .finally(() => {
+                    loading.value = false;
+                    // 加载完毕后，无论成功与否都要清除延迟定时器
+                    clearTimeout(loadingTimer);
+                })
+         
+        
 
             let timer = null;
             if (options.timeout) {
@@ -103,7 +139,7 @@ function defineAsyncComponent(options){
             //     return loaded.value 
             //                 ?  { type: InnerComp } 
             //                 :  { type: Text, children: '' }
-            // }
+            }
         }
     }
 }

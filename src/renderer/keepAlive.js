3592b14d9b3806c3af2f98b01c976d9963e60656
Rhodri-hc <13622450494@163.com>
2022-07-02 17:49:40 +0800

    LRU算法

--- src/renderer/keepAlive.js
+++ src/renderer/keepAlive.js
@@ -84,3 +84,42 @@ const KeepAlive = {
         } 
     }
 }
+
+/**
+* @desc LRU (Least Recently Used) 算法 '最近最少使用'
+* @author 张和潮
+* @date 2022年07月02日 12:08:21
+*/
+class LRUCache {
+    constructor( len ) {
+      this.length = len; // 存储长度
+      this.data = new Map(); // 存储数据
+    }
+    // 存储数据，通过键值对的方式
+    set(key, value) {
+      const data = this.data;
+      if (data.has(key)) {
+        data.delete(key)
+      }
+      data.set(key, value);
+
+
+      // 如果超出了容量，则需要删除最久的数据
+      if (data.size > this.length) {
+        const delKey = data.keys().next().value;
+        data.delete(delKey);
+      }
+    }
+    // 获取数据
+    get(key) {
+      const data = this.data;
+      // 未找到
+      if (!data.has(key)) {
+        return null;
+      }
+      const value = data.get(key); // 获取元素
+      data.delete(key); // 删除元素
+      data.set(key, value); // 重新插入元素
+    }
+  }
+  const lruCache = new LRUCache(5);

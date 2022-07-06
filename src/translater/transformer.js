e4c5885ed85ebfff30e56fa2c789e27f72feb879
Rhodri-hc <13622450494@163.com>
2022-07-06 14:39:44 +0800

    将模板AST 转换为 JavaScript AST

--- src/translater/transformer.js
+++ src/translater/transformer.js
@@ -100,7 +100,9 @@ function transform(ast) {
             // 转换标签节点
             transformElement,
             // 转换文本节点
-            transformText
+            transformText,
+            // 转换为根节点
+            transformRoot
         ],
         // 用来替换节点
         replaceNode(node){
@@ -131,8 +133,31 @@ function transform(ast) {
 * @date 2022年07月05日 22:42
 */
 function transformElement(node) {
-    if (node.type === 'Element' && node.tag === 'p') {
-        node.tag = 'h1'
+    // 将转换代码编写在退出阶段的回调函数中，
+    // 这样可以保证该标签节点的子节点全部被处理完毕
+    return () => {
+        // 如果被转换的节点不是元素节点，则什么都不做
+        if (node.type !== 'Element') {
+            return;
+        }
+
+        // 1. 创建 h 函数调用语句，
+        //    h 函数调用第一个参数是标签名称，因此我们以 node.tag 来创建一个字符串字面量节点
+        //    作为第一个参数
+        const callExp = createCallExpression('h', [
+            createStringLiteral(node.tag)
+        ])
+        // 2. 处理 h 函数调用的参数
+        node.children.length === 1
+            // 如果当前标签节点只有一个子节点，则直接使用子节点的jsNode 作为参数
+            ? callExp.arguments.push(node.children[0].jsNode)
+            // 如果当前标签节点有多个子节点，则创建一个 ArrayExpression 节点作为参数
+            : callExp.arguments.push(
+                // 数组的每个元素都是子节点的 jsNode
+                createArrayExpression(node.children.map(c => c.jsNode))
+            )
+        // 3. 将当前标签节点对应的 JavaScript AST 添加到jsNode 属性下
+        node.jsNode = callExp;
     }
 }
 /**
@@ -141,7 +166,94 @@ function transformElement(node) {
 * @date 2022年07月05日 22:42
 */
 function transformText(node, context) {
-    if (node.type === 'Text') {
-        context.removeNode()
+    if (node.type !== 'Text') {
+        return;
+    }
+
+    // 文本节点对应的 JavaScript AST 节点其实就是一个字符创字面量，
+    // 因此只需要使用 node.content 创建一个 StringLiteral 类型的节点即可
+    // 最后将文本节点对应的 JavaScript AST 节点添加到 node.jsNode 属性下
+    node.jsNode = createStringLiteral(node.content);
+}
+
+/**
+* @desc 转换Root 根节点
+* @author 张和潮
+* @date 2022年07月06日 14:14:46
+*/
+function transformRoot(node) {
+    // 将逻辑编写在退出阶段的回调函数中，保证自己诶单全部被处理完毕
+    return ()=> {
+        // 如果不是根节点，则什么都不做
+        if (node.type !== 'Root') {
+            return;
+        }
+
+        // node 是根节点，根节点的第一个子节点就是模板的根节点，
+        // 当然，这里我们暂时不考虑模板存在多个根节点的情况
+        const vnodeJSAST = node.children[0].jsNode
+        // 创建 render 函数的声明语句节点，将vnodeJSAST 作为render 函数体返回语句
+        node.jsNode = {
+            type: 'FunctionDecl',
+            id: { type: 'Identifier', name: 'render' },
+            params: [],
+            body:[
+                {
+                    type: 'ReturnStatement',
+                    return: vnodeJSAST
+                }
+            ]
+        }
+    }
+}
+
+/***************************************** 以下函数用于创建 对应JavaScript AST节点 *************************************/
+
+/**
+* @desc 用来创建 StringLiteral 节点
+* @author 张和潮
+* @date 2022年07月06日 10:07:59
+*/
+function createStringLiteral(value) {
+    return {
+        type: 'StringLiteral',
+        value
+    }
+}
+
+/**
+* @desc 用来创建 Identifier 节点
+* @author 张和潮
+* @date 2022年07月06日 11:51:04
+*/
+function createIdentifier(name) {
+    return {
+        type: 'Identifier',
+        name
+    }
+}
+
+/**
+* @desc 用来创建 ArrayExpression 节点
+* @author 张和潮
+* @date 2022年07月06日 11:53:43
+*/
+function createArrayExpression(elements) {
+    return {
+        type: 'ArrayExpression',
+        elements
+    }
+}
+
+/**
+* @desc 用来创建 CallExpression 节点
+* @author 张和潮
+* @date 2022年07月06日 11:57:07
+*/
+function createCallExpression(callee, arguments) {
+    return{
+        type: 'CallExpression',
+        callee: createIdentifier(callee),
+        arguments
     }
 }

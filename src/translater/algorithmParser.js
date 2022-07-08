e90682da3701338f3fb2be18e37d82a4048445b4
Rhodri-hc <13622450494@163.com>
2022-07-08 22:45:46 +0800

    递归下降算法

--- src/translater/algorithmParser.js
+++ src/translater/algorithmParser.js
@@ -38,3 +38,76 @@ function algoParse(str) {
         children: nodes
     }
 }
+
+/**
+* @desc 解析函数
+* @author 张和潮
+* @date 2022年07月08日 21:56
+*/
+function parseChildren(context, ancestors){
+    // 定义 nodes 数组存储子节点，它将作为最终的返回值
+    let nodes = [];
+    // 从上下文对象化中取得当前状态，包括模式 mode 和模板内容source
+    const { mode, source } = context;
+    // 开启while 循环，只要满足条件就会一直对字符串进行解析
+    while (!isEnd(context, ancestors)) {
+        let node;
+        // 只有 DATA模式 和 RCDATA 模式下才支持插值节点的解析
+        if (mode === TextModes.DATA || mode === TextModes.RCDATA) {
+            // 只有DATA 模式才支持标签节点的解析
+            if (mode === TextModes.DATA && source[0] === '<') {
+                if (source[1] === '!') {
+                    if (source.startsWith('<!--')) {
+                        // 注释
+                        node = parseComment(context);
+                    } else if(source.startsWith('<![CDATA[')){
+                        // CDATA
+                        node = parseCDATA(context, ancestors)
+                    } 
+
+                } else if (source[1] === '/') {
+                    // 结束标签
+                } else if (/[a-z]/i.test(source[1])){
+                    // 标签
+                    node = parseElement(context, ancestors)
+                }
+
+            } else if (source.startsWith('{{')) {
+                // 解析插值
+                node = parseInterpolation(context)
+            }
+        }
+
+        // node 不存在，说明处于其他模式，即非 DATA 且非 RCDATA 模式
+        // 当做文本处理
+        if (!node) {
+            // 解析文本节点
+            node = parseText(context)
+        }
+
+
+        // 将节点添加到 nodes 数组中
+        nodes.push(node);
+    }
+
+    // 循环结束后，说明子节点解析完毕，返回子节点
+    return nodes;
+}
+
+/**
+* @desc 解析标签
+* @author 张和潮
+* @date 2022年07月08日 22:35
+*/
+function parseElement(context, ancestors) {
+    // 解析开始标签
+    const element = parseTag(context, ancestors);
+
+    // 递归调用parseChildren 函数进行 <div> 标签子节点的解析
+    element.children = parseChildren(context, ancestors)
+
+    // 解析结束标签
+    parseEndTag(context, ancestors)
+
+    return element;
+}

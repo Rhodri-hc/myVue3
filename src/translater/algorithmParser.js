3b307823bd48529c0f8c5438a0be09f32824b321
Rhodri-hc <13622450494@163.com>
2022-07-10 18:51:37 +0800

    解析文本

--- src/translater/algorithmParser.js
+++ src/translater/algorithmParser.js
@@ -300,3 +300,40 @@ function parseAttributes(context){
     // 将解析结果返回
     return props;
 }
+
+/**
+* @desc 解析文本
+* @author 张和潮
+* @date 2022年07月10日 17:54
+*/
+function parseText(context) {
+    // endIndex 为文本内容的结尾索引，默认将整个模板剩余内容作为文本内容
+    let endIndex = context.source.length
+    // 寻找字符 < 的位置索引
+    const ltIndex = context.source.indexOf('<');
+    // 寻找定界符 {{ 的位置索引
+    const delimiterIndex = context.source.indexOf('{{');
+
+    // 取 ltIndex 和当前 endIndex 中较小的一个作为新的结尾索引
+    if (ltIndex > -1 && ltIndex < endIndex) {
+        endIndex = ltIndex;
+    }
+
+    // 取 delimiterIndex 和当前 endIndex 中较小的一个作为新的结尾索引
+    if (delimiterIndex > -1 && delimiterIndex < endIndex) {
+        endIndex = delimiterIndex;
+    }
+
+    // 调用slice 函数截取文本内容
+    const content = context.source.slice(0, endIndex);
+    // 消耗文本内容
+    context.advanceBy(context.length)
+
+    // 返回文本节点
+    return {
+        // 节点类型
+        type: 'Text',
+        // 文本内容
+        content
+    }
+}
